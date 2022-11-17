4a18f52193b087a76ff8ed8930f4aecf40325c8f
Abhay <abhaykvincent@gmail.com>
2022-11-16 20:08:12 -0500

    scss integrated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
 
       {/* Header */}
-      <header>
-        <div className="logo"></div>
-      </header>
+      <Header/>
 
       <main>
 
