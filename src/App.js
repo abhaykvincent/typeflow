dc9676fd8150d0c016cd9319699517ee63bae38c
Abhay <abhaykvincent@gmail.com>
2022-11-16 19:51:47 -0500

    basic HTML structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      {/* Header */}
+      <header>
+        Header
+      </header>
+
+      <main>
+
+        {/* Source Code */}
+        <div className="source-code">
+        </div>
+        {/* Playground */}
+        <div className="playground">
+        </div>
+        
+      </main>
+      
+      {/* Command Control */}
+      <div className="command-control">
+        <button>Button</button>
+      </div>
     </div>
   );
 }
