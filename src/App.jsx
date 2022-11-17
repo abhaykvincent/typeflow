99eff9b9048c4814da3f0dd0d45a37314993bdb7
Abhay <abhaykvincent@gmail.com>
2022-11-16 20:02:54 -0500

    basic structure and labels ready for styling

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,19 @@ function App() {
         <div className="source-code">
           <div className="source-code__header">
             <h1>Source Code</h1>
+            <div className="source-code__header-options">
+              <div className="option-button copy"> copy</div>
+              <div className="option-button delete"> delete</div>
+            </div>
           </div>
           <div className="source-code__body">
+
+            {/* Final Code */}
+            <pre>
+              <code>
+                {`import React from 'react';`}
+              </code>
+            </pre>
           </div>
         </div>
 
@@ -29,6 +40,28 @@ function App() {
             <h1>Playground</h1>
           </div>
           <div className="playground__body">
+
+            <div className="code-snippets">
+              {/*  Code Snippets */}
+            <pre>
+              <code>
+                {`import React from 'react';`}
+              </code>
+              <code>
+                {`import logo from './logo.svg';`}
+              </code>
+              <code>
+                {`import './App.css';`}
+              </code>
+              <code>
+                {`function App() {`}
+              </code>
+              <code>
+                {`  return (`}
+              </code>
+              
+            </pre>
+            </div>
           </div>
         </div>
         
@@ -36,7 +69,16 @@ function App() {
 
       {/* Command Control */}
       <div className="command-control">
-        <div className="language-settings"></div>
+        <div className="language-settings">
+          <label htmlFor="language">Language : </label>
+          <select name="language" id="language-select">
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="c++">C++</option>
+          </select>
+
+        </div>
         <div className="command-input__wrapper">
           <input type="text" className="command-input" placeholder="Enter a command" />
           <button className="command-input__button">Run</button>
